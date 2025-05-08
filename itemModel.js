17771df927cb1293fd068c136c039609e5fbddb0
Oluwatosin Oguntoyinbo <oluwatosinoguntoyinboo@gmail.com>
2025-05-08 23:52:49 +0100

    Added the export statement for the item Model
    Still Work In Progress

--- itemModel.js
+++ itemModel.js
@@ -26,3 +26,6 @@ const itemSchema = new mongoose.Schema({
 const Item = mongoose.model("Item", itemSchema); //this is the model that is used to create the 
 // collection in the mongodb database. It is used to create the collection and it takes in the name of 
 // the collection and the schema that is used to create the collection.
+
+module.exports = Item; //this is the export statement that is used to export the model 
+// so that it can be used in other files. It is used to export the model so that it can be used in other files.
